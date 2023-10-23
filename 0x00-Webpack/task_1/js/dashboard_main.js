9a2bc2c4454b78635ef21c05e69de626e87b64c3
Thecodingyogi <cynthiatonui@gmail.com>
2023-10-23 17:56:22 +0000

    Adding CSS & Images

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,20 +1,19 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-
-const countParagraph = $('#count');
-const button = $('<button>').text('Click here to get started');
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append(button);
-$('body').append(countParagraph);
-$('body').append('<p>Copyright - Holberton School</p>');
+$('body').append(`
+ <p>Holberton Dashboard</p>
+ <p>Dashboard data for the students</p>
+ <button>Click here to get started</button>
+ <p id='count'></p>
+ <p>Copyright - Holberton School</p>
+`);
 
 let count = 0;
 
 function updateCounter() {
   count++;
-  countParagraph.text(`${count} clicks on the button`);
+  $('#count').text(`${count} clicks on the button`);
 }
 
-button.on('click', _.debounce(updateCounter, 1000));
+$('button').on('click', _.debounce(updateCounter, 1000));
