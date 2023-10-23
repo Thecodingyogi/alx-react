e386e4d43ded7bacd345204e8ed3634dad9483be
Thecodingyogi <cynthiatonui@gmail.com>
2023-10-23 16:36:14 +0000

    Updated the config file.

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 
 module.exports = {
-  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  mode: 'production',
+  entry: './js/dashboard_main.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  mode: 'production'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  }
 };
