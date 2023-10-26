6121d4365c39cac12fe9165bd5452c38a3b76df1
Thecodingyogi <cynthiatonui@gmail.com>
2023-10-26 13:59:23 +0300

    React Tasks

--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div className='root-notifications'>
+	<Notifications />
+    </div>
   </React.StrictMode>
 );
 
---
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Notifications.css';
+import {getLatestNotification} from './utils';
+import closeIcon from './close-icon.png';
+
+function Notifications() {
+  return (
+    <>
+      <div className="Notifications">
+        <p>Here is the list of notifications</p>
+	<ul>
+	  <li data-priority="default">New course available</li>
+	  <li data-priority="urgent">New resume available</li>
+	  <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+	</ul>
+	<button
+	  style={{
+            float: 'right',
+	    backgroundColor: 'transparent',
+	    border: 'none',
+	    cursor: 'pointer',
+	  }}
+	  aria-label="Close"
+	  onClick={() => console.log('Close button has been clicked')}
+        >
+	  <img src={closeIcon} alt="Close Icon" />
+	</button>
+      </div>
+    </>
+  );
+}
+
+export default Notifications;
